57d6ec52de49cc1b7c5ae06634d673295f9b0f1f
root <root@ip-172-31-85-106.ec2.internal>
2022-12-04 07:42:03 +0000

    second

--- automation.sh
+++ automation.sh
@@ -1,29 +1,29 @@
+
 #!/bin/bash
 
-name="Nikita"
+
+name="nikita"
 s3_bucket="upgrad-nikita"
 
 
 apt update -y
 
-if [[ apache2 != $(dpkg --get-selections apache2 | awk '{print $1}') ]]; then
-	#statements
+
+if [[ apache2 != $(apt --get-selections apache2 | awk '{print $1}') ]]; then
 	apt install apache2 -y
 fi
 
+
 running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()')
 if [[ running != ${running} ]]; then
-	#statements
 	systemctl start apache2
 fi
 
 
 enabled=$(systemctl is-enabled apache2 | grep "enabled")
 if [[ enabled != ${enabled} ]]; then
-	#statements
 	systemctl enable apache2
 fi
-
 
 timestamp=$(date '+%d%m%Y-%H%M%S')
 
@@ -31,7 +31,26 @@ timestamp=$(date '+%d%m%Y-%H%M%S')
 cd /var/log/apache2
 tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 
+
 if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]]; then
-	#statements
+	
 	aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
+fi
+
+document="/var/www/html"
+
+if [[ ! -f ${document}/inventory.html ]]; then
+
+	echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${document}/inventory.html 
+fi
+
+if [[ -f ${document}/inventory.html ]]; then
+	
+    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${document}/inventory.html 
+fi
+
+if [[ ! -f /etc/cron.d/automation ]]; then
+
+	echo " 0 1 * * * root /root/automation.sh" >> /etc/cron.d/automation
 fi
